4443c79db84739d1537f2fd9c8031e63886f7f41
assiaSannaghi <assia.sannaghi>
2026-06-11 17:47:47 +0100

    theme store

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Header from './components/layout/Header.vue'
+import Footer from './components/layout/Footer.vue'
 </script>
+
 <template>
-  hello
-  <RouterView />
+  <div class="d-flex flex-column min-vh-100">
+    <Header></Header>
+    <div class="flex-grow-1">
+      <RouterView />
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
